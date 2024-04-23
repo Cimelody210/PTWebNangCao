82e98902a81a429b1b206c5a274a165b74f2e28a
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-23 14:08:49 +0700

    dgh

---
+++ LinkPhongDu/Codenaodo/demo.cs
@@ -0,0 +1,24 @@
+
+
+public tbNguoiDung GetById(string username)
+{
+    return db.tbNguoiDung.SingleOrDefault(x => x.TaiKhoan == username);
+    
+}
+public int Login(string username, string password, bool isLoginAdmin =false)
+{
+    var result = db.tbNguoiDung.SingleOrDefault(x => x.TaiKhoan == username);
+    if(result != null)
+    {
+        return 0;
+    }
+    else{
+        if(isLoginAdmin== true)
+        {
+            if(result.MaBP == CommonConstants.ADMIN_GROUP || result.MaBP = ADMIN_GROUP.Ten){
+                if(result.TrangThai == false)
+                    return -1;
+            }
+        }
+    }
+}
