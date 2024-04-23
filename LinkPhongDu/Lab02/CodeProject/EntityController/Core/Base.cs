d1611dd0277edeeb2ff7222f140d40fed0479e0c
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-23 17:44:07 +0700

    Add files via upload

---
+++ LinkPhongDu/Lab02/CodeProject/EntityController/Core/Base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityModel;
+namespace EntityController.Core
+{
+	public abstract class Base
+	{
+		public EF db;
+		public Base()
+		{
+			db = new EF();
+		}
+	}
+}
