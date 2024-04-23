bbed9fcac6136ae093bc2ce3cd132245e52b1879
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-23 17:42:17 +0700

    Add files via upload

---
+++ LinkPhongDu/Lab02/CodeProject/Application/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EntityModel;
+using EntityController;
+
+namespace Application
+{
+	public class Controller
+	{
+		
+	}
+}
