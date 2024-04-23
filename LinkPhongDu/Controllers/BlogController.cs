114d2041cc7f3c3e393b951a4b3a4254f79e6ac8
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-23 17:03:39 +0700

    Add files via upload

---
+++ LinkPhongDu/Controllers/BlogController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EntityController;
+using EntityModel;
+namespace MvcBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private EF db= new EF();
+        // GET: Blog
+        public string Index()
+        {
+			string result = "";
+			foreach (BlogSet blog in db.BlogSets)
+			{
+				result += "\n" + blog.BlogId + "\t " + blog.Name + "\t" + blog.Url;
+			}
+			return result;
+		}
+
+		// GET: BlogSets/Get/5
+		public string Get(int id)
+		{
+			BlogSet result = db.BlogSets.Where(blog => blog.BlogId == id).FirstOrDefault();
+			return result.Name + " " + result.Url;
+		}
+
+	}
+}
