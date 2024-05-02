25d36cd778c35aee66c39a996285a5edbc29b208
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-05-02 09:55:56 +0700

    dhdh

--- LinkPhongDu/Codenaodo/demo.cs
+++ LinkPhongDu/Codenaodo/demo.cs
@@ -1,26 +1,40 @@
 using System;
 using System.IO;
 using System.Collection.Generic;
+using System.Linq;
+using System.Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Microsoft.Extensions.Logging;
 
-
-public tbNguoiDung GetById(string username)
+namespace demo
 {
-    return db.tbNguoiDung.SingleOrDefault(x => x.TaiKhoan == username);
-    
-}
-public int Login(string username, string password, bool isLoginAdmin =false)
-{
-    var result = db.tbNguoiDung.SingleOrDefault(x => x.TaiKhoan == username);
-    if(result != null)
+    public class IndexModel: PageModel1
     {
-        return 0;
-    }
-    else{
-        if(isLoginAdmin== true)
+        private readonly ILogger<IndexModel> _logger;
+        public IndexModel(ILogger<IndexModel> logger)
         {
-            if(result.MaBP == CommonConstants.ADMIN_GROUP || result.MaBP = ADMIN_GROUP.Ten){
-                if(result.TrangThai == false)
-                    return -1;
+
+        }
+
+    }
+    public tbNguoiDung GetById(string username)
+    {
+        return db.tbNguoiDung.SingleOrDefault(x => x.TaiKhoan == username);
+        
+    }
+    public int Login(string username, string password, bool isLoginAdmin =false)
+    {
+        var result = db.tbNguoiDung.SingleOrDefault(x => x.TaiKhoan == username);
+        if(result != null)
+        {
+            return 0;
+        }
+        else{
+            if(isLoginAdmin== true)
+            {
+                if(result.MaBP == CommonConstants.ADMIN_GROUP || result.MaBP = ADMIN_GROUP.Ten){
+                    if(result.TrangThai == false)
+                        return -1;
+                }
             }
         }
     }
